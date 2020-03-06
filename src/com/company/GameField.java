2a3d9fb81e8528046ae614266a49bf131486f1e1
Бондарь Николай <nik_bondar97@mail.ru>
2020-03-06 18:42:11 +0300

    commit

---
+++ src/com/company/GameField.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameField extends JPanel {
+    private final int SIZE = 320;
+    private final int DOT_SIZE = 16;
+    private final int ALL_DOTS = 400;
+    private Image dot, apple;
+    private int appleX, appleY;
+    private 
+}
