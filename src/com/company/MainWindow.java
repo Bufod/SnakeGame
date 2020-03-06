34958c40447571bbeb404ddbc96e495f06de01e8
Бондарь Николай <nik_bondar97@mail.ru>
2020-03-06 18:35:19 +0300

    commit

--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -7,5 +7,11 @@ public class MainWindow extends JFrame {
 
     public MainWindow(){
         setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400, 400);
+        setResizable(false);
+        add(new GameField());
+        setVisible(true);
     }
 }
