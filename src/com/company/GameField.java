58646371f22dba2ad22b1e113ec1e9b1932b3900
Бондарь Николай <nik_bondar97@mail.ru>
2020-03-06 19:01:21 +0300

    commit

--- src/com/company/GameField.java
+++ src/com/company/GameField.java
@@ -9,5 +9,25 @@ public class GameField extends JPanel {
     private final int ALL_DOTS = 400;
     private Image dot, apple;
     private int appleX, appleY;
-    private 
+    private int[] x = new int[ALL_DOTS];
+    private int[] y = new int[ALL_DOTS];
+    private int dots;
+    private Timer timer;
+    private boolean left = false,
+            right = true,
+            up = false,
+            down = true;
+    private boolean inGame = true;
+
+    public GameField() {
+        setBackground(Color.BLACK);
+        loadImages();
+        initGames();
+        addKeyListener();
+        setFocusable(true);
+    }
+
+    private void loadImages() {
+        
+    }
 }
